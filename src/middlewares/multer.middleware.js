26ab4f3fc56faf676f34b5b684d4b5af0bc34ceb
Goutam Dogri <dogrig507@gmail.com>
2024-06-30 16:45:10 +0530

    file size limit increased to 50mb

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -13,6 +13,6 @@ const storage = multer.diskStorage({
 export const upload = multer({
     storage, // storage: storage
     limits: {
-        fileSize: 1024 * 1024 * 5
+        fileSize: 1024 * 1024 * 50
     }
 })
